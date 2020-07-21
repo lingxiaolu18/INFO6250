d1b475a12b45c99b58ef4400428a549481604be7
An indifferent coding mechine <sf@Ans-MacBook-Pro.local>
2020-07-21 00:43:23 -0700

    add build

---
+++ work/react-chat/chat/build/precache-manifest.e4b4223c46ca1165e58cdf8b925bdef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20cf328833440c6c5811230593c69153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7af68d46965486197c23",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "297140ca78ff6d501f71",
+    "url": "/static/js/2.7253ce88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7253ce88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af68d46965486197c23",
+    "url": "/static/js/main.12ab423b.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  }
+]);
